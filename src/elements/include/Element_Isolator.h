0041d2a8662d1307c15eb66e667a04ba92232755
jvick3 <jvick3@unm.edu>
2014-11-24 17:52:40 -0700

    Made Isolator diffusable so that it can perform tight (Inner Radius 1) bubbles

--- src/elements/include/Element_Isolator.h
+++ src/elements/include/Element_Isolator.h
@@ -138,15 +138,13 @@ namespace MFM
 
      virtual u32 Diffusability(EventWindow<CC> & ew, SPoint nowAt, SPoint maybeAt) const
      {
-      return nowAt.Equals(maybeAt) ? 
-                                    Element<CC>::COMPLETE_DIFFUSABILITY 
-                                    : 0;
+       return Element<CC>::COMPLETE_DIFFUSABILITY ;
      }
 
     virtual u32 PercentMovable(const T& you,
                                const T& me, const SPoint& offset) const
     {
-      return 0;
+      return 100;
     }
 
     virtual void Behavior(EventWindow<CC>& window) const
